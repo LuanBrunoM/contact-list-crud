4dbc227ac7fbdefcbae3f7e445d6b8fe9e58d8fd
Luan Bruno <luanbrunomelo03@gmail.com>
2023-08-30 19:38:09 -0300

    style update

--- index.php
+++ index.php
@@ -12,22 +12,22 @@ include_once("./templates/header.php");
         <tr>
           <th scope="col">#</th>
           <th scope="col">Nome</th>
-          <th scope="col">telefone</th>
+          <th scope="col">Telefone</th>
           <th scope="col">email</th>
-          <th scope="col">#</th>
+          <th scope="col"></th>
         </tr>
       </thead>
       <tbody>
         <?php foreach ($contacts as $contact) : ?>
           <tr>
-            <td scope="row"><?= $contact["id"] ?></td>
+            <td scope="row" class="col-id"><?= $contact["id"] ?></td>
             <td scope="row"><?= $contact["name"] ?></td>
             <td scope="row"><?= $contact["phone"] ?></td>
             <td scope="row"><?= $contact["email"] ?></td>
             <td class="actions">
               <a href="#"><i class="fas fa-eye check-icon"></i></a>
               <a href="#"><i class="far fa-edit edit-icon"></i></a>
-              <button type="submit"><i class="fas fa-times delete-icon"></i></button>
+              <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
             </td>
           </tr>
         <?php endforeach; ?>
