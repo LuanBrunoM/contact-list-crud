c821c1267121ded168d5bf52077e3ba1a00f7b9e
Luan Bruno <luanbrunomelo03@gmail.com>
2023-08-31 11:24:53 -0300

    show page

--- config/process.php
+++ config/process.php
@@ -5,12 +5,33 @@ session_start();
 include_once("conection.php");
 include_once("url.php");
 
-$contacts = [];
+$id;
 
-$query = "SELECT * FROM contacts";
+if (!empty($_GET)) {
+  $id = $_GET["id"];
+}
 
-$stmt = $conn->prepare($query);
+//Retorno os dados de um contato por vez
+if (!empty($id)) {
 
-$stmt->execute();
+  $query = "SELECT * FROM contacts WHERE id = :id";
 
-$contacts = $stmt->fetchAll();
+  $stmt = $conn->prepare($query);
+
+  $stmt->bindParam(":id", $id);
+
+  $stmt->execute();
+
+  $contact = $stmt->fetch();
+} else {
+  //Retorno dos contatos
+  $contacts = [];
+
+  $query = "SELECT * FROM contacts";
+
+  $stmt = $conn->prepare($query);
+
+  $stmt->execute();
+
+  $contacts = $stmt->fetchAll();
+}
