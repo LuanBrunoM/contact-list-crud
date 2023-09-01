772ae56df82feb5ae0d9af53ba3e7ddbd67ace28
Luan Bruno <luanbrunomelo03@gmail.com>
2023-09-01 13:50:31 -0300

    create and edit

--- config/process.php
+++ config/process.php
@@ -5,33 +5,98 @@ session_start();
 include_once("conection.php");
 include_once("url.php");
 
-$id;
+$data = $_POST;
+//modificações no banco
+if (!empty($data)) {
 
-if (!empty($_GET)) {
-  $id = $_GET["id"];
-}
+  //criar o contato
+  if ($data["type"] === "create") {
+    $name = $data["name"];
+    $phone = $data["phone"];
+    $email = $data["email"];
+    $observations = $data["observations"];
 
-//Retorno os dados de um contato por vez
-if (!empty($id)) {
+    $query = "INSERT INTO contacts (name, phone, email, observations) VALUES (:name, :phone, :email, :observations)";
 
-  $query = "SELECT * FROM contacts WHERE id = :id";
+    $stmt = $conn->prepare($query);
 
-  $stmt = $conn->prepare($query);
+    $stmt->bindParam(":name", $name);
+    $stmt->bindParam(":phone", $phone);
+    $stmt->bindParam(":email", $email);
+    $stmt->bindParam(":observations", $observations);
 
-  $stmt->bindParam(":id", $id);
+    try {
+      $stmt->execute();
+      $_SESSION["msg"] = "Novo contato criado com sucesso";
+    } catch (PDOException $e) {
+      // erro na conexão
+      $error = $e->getMessage();
+      echo "Erro: $error";
+    }
+  } else if ($data["type"] === "edit") {
+    $name = $data["name"];
+    $phone = $data["phone"];
+    $email = $data["email"];
+    $observations = $data["observations"];
+    $id = $data["id"];
 
-  $stmt->execute();
+    $query = "UPDATE contacts SET name = :name, phone = :phone, email = :email, observations = :observations WHERE id = :id";
 
-  $contact = $stmt->fetch();
+    $stmt = $conn->prepare($query);
+
+    $stmt->bindParam(":name", $name);
+    $stmt->bindParam(":phone", $phone);
+    $stmt->bindParam(":email", $email);
+    $stmt->bindParam(":observations", $observations);
+    $stmt->bindParam(":id", $id);
+
+    try {
+      $stmt->execute();
+      $_SESSION["msg"] = "Contato editado com sucesso";
+    } catch (PDOException $e) {
+      // erro na conexão
+      $error = $e->getMessage();
+      echo "Erro: $error";
+    }
+  }
+
+  //Voltar para home depois do cadastro
+  header("Location:" . $BASE_URL . "../index.php");
+
+  // Seleção de dados
 } else {
-  //Retorno dos contatos
-  $contacts = [];
 
-  $query = "SELECT * FROM contacts";
+  $id;
 
-  $stmt = $conn->prepare($query);
+  if (!empty($_GET)) {
+    $id = $_GET["id"];
+  }
 
-  $stmt->execute();
+  //Retorno os dados de um contato por vez
+  if (!empty($id)) {
 
-  $contacts = $stmt->fetchAll();
+    $query = "SELECT * FROM contacts WHERE id = :id";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->bindParam(":id", $id);
+
+    $stmt->execute();
+
+    $contact = $stmt->fetch();
+  } else {
+    //Retorno dos contatos
+    $contacts = [];
+
+    $query = "SELECT * FROM contacts";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->execute();
+
+    $contacts = $stmt->fetchAll();
+  }
 }
+
+//Fechar conecção
+$cnn = null;
--- templates/header.php
+++ templates/header.php
@@ -29,7 +29,7 @@ if (isset($_SESSION['msg'])) {
       <div>
         <div class="navbar-nav">
           <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>/index.php">Contatos</a>
-          <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>/create.php">Novo Contato</a>
+          <a class="nav-link active" href="<?= $BASE_URL ?>/create.php">Novo Contato</a>
         </div>
       </div>
     </nav>
